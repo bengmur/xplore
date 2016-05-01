5a4e4b062d7ebc74c37974d25361dc1b0aecff4f
Ryan Dorson <slyryd@gmail.com>
2016-05-01 18:58:48 -0400

    Combine user destination and four destinations code.

--- app/src/main/java/edu/umd/cs/xplore/PlanActivity.java
+++ app/src/main/java/edu/umd/cs/xplore/PlanActivity.java
@@ -1,23 +1,12 @@
 package edu.umd.cs.xplore;
 
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.NumberPicker;
-import android.widget.TimePicker;
-import android.widget.Toast;
 
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Place;
@@ -34,10 +23,6 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
-
-import javax.net.ssl.HttpsURLConnection;
-
-import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 
@@ -47,8 +32,8 @@ public class PlanActivity extends AppCompatActivity {
 
     private NumberPicker hourField;
     private NumberPicker minuteField;
-
-    private static final String TAG = "planActivity";
+    private PlaceAutocompleteFragment autocompleteFragment;
+    private Place destination = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,17 +53,14 @@ public class PlanActivity extends AppCompatActivity {
                 return String.format(Locale.ENGLISH, "%02d", value);
             }
         };
-
         hourField.setFormatter(formatter);
         minuteField.setFormatter(formatter);
         hourField.setValue(6);
         minuteField.setValue(30);
 
+        // Setup autocomplete fragment
         autocompleteFragment = (PlaceAutocompleteFragment)
                 getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
-
-//        final TextView txtPlaceDetails = (TextView) findViewById(R.id.txt_place_details);
-
         autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(Place place) {
@@ -86,12 +68,11 @@ public class PlanActivity extends AppCompatActivity {
                 Log.i(TAG, "Place: " + place.getName());
 
                 destination = place;
-//                StringBuilder sb = new StringBuilder();
-//                sb.append("<b>").append(place.getName()).append("</b>").append("\n");
-//                sb.append(place.getAddress()).append("\n");
-//                sb.append(place.getAttributions() == null ? "" : place.getAttributions());
-//                txtPlaceDetails.setText(Html.fromHtml(sb.toString()));
-//                txtPlaceDetails.setText(autocompleteFragment.toString());
+                // place.getName();
+                // place.getId();
+                // place.getLatLng();
+                // place.getAddress();
+                // place.getAttributions();
             }
 
             @Override
@@ -100,10 +81,9 @@ public class PlanActivity extends AppCompatActivity {
                 Log.i(TAG, "An error occurred: " + status);
             }
         });
-
-//        autocompleteFragment.setBoundsBias(new LatLngBounds(
-//                new LatLng(-33.880490, 151.184363),
-//                new LatLng(-33.858754, 151.229596)));
+        // autocompleteFragment.setBoundsBias(new LatLngBounds(
+        //         new LatLng(-33.880490, 151.184363),
+        //         new LatLng(-33.858754, 151.229596)));
     }
 
     public void fabClicked(View view) {
@@ -112,8 +92,6 @@ public class PlanActivity extends AppCompatActivity {
         int minutes = minuteField.getValue();
 
         // Get destination
-//        EditText destField = (EditText) findViewById(R.id.destination);
-//        String inputDest = destField.getText().toString();
         String inputDest = (destination == null ? "" : destination.getName().toString());
         ArrayList<String> destinations = new ArrayList<String>();
 
@@ -127,21 +105,21 @@ public class PlanActivity extends AppCompatActivity {
             double currLat = 39.4143;
             double currLong = -77.4105;
 
-                    /* Using duration, limit travel to final destination and back to origin to 40%
-                        of duration, so 20% for each way, assuming speed of 60mph. Total duration in
-                        minutes means total possible miles we can drive in this trip. */
+            /* Using duration, limit travel to final destination and back to origin to 40%
+               of duration, so 20% for each way, assuming speed of 60mph. Total duration in
+               minutes means total possible miles we can drive in this trip. */
             int totalDuration = (60 * hours) + minutes;
             double radius = totalDuration / 5;
 
-                    /* Using radius, we can figure out how much the lat and long of the origin
-                      can change. 1 degree change in lat is 69 miles, and 1 degree in long is 53 miles. */
+            /* Using radius, we can figure out how much the lat and long of the origin
+               can change. 1 degree change in lat is 69 miles, and 1 degree in long is 53 miles. */
             double latDelta = radius / 69;
             double longDelta = radius / 53;
 
-                    /* Create an array for 4 different LatLng coordinates that the user can go during
-                        this time. Each LatLng coordinate is 2 consecutive elements, one for Lat and one
-                        for Long. Use the Geonames service in an AsyncTask to find the name of
-                        each calculated location. */
+            /* Create an array for 4 different LatLng coordinates that the user can go during
+               this time. Each LatLng coordinate is 2 consecutive elements, one for Lat and one
+               for Long. Use the Geonames service in an AsyncTask to find the name of
+               each calculated location. */
             String[] coordinates = {Double.toString(currLat + latDelta), Double.toString(currLong),
                     Double.toString(currLat - latDelta), Double.toString(currLong),
                     Double.toString(currLat), Double.toString(currLong + longDelta),
@@ -153,14 +131,14 @@ public class PlanActivity extends AppCompatActivity {
         } else {
             // Add only user destination
             destinations.add(inputDest);
-        }
 
-        // Start preferences activity, while passing down destinations data
-        Intent preferencesIntent = new Intent(getApplicationContext(), PreferencesActivity.class);
-        preferencesIntent.setAction(Intent.ACTION_SEND_MULTIPLE);
-        preferencesIntent.putStringArrayListExtra(Intent.EXTRA_STREAM, destinations);
-        preferencesIntent.setType("possibleDestinations");
-        startActivity(preferencesIntent);
+            // Start preferences activity, while passing down destinations data
+            Intent preferencesIntent = new Intent(getApplicationContext(), PreferencesActivity.class);
+            preferencesIntent.setAction(Intent.ACTION_SEND_MULTIPLE);
+            preferencesIntent.putStringArrayListExtra(Intent.EXTRA_STREAM, destinations);
+            preferencesIntent.setType("possibleDestinations");
+            startActivity(preferencesIntent);
+        }
     }
 
     private String readStream(InputStream is) {
@@ -204,9 +182,9 @@ public class PlanActivity extends AppCompatActivity {
 
                     URL reqURL = new URL(urlStringBuilder.toString());
                     HttpURLConnection urlConnection = (HttpURLConnection) reqURL.openConnection();
-                    urlConnections[i/2] = urlConnection;
+                    urlConnections[i / 2] = urlConnection;
 
-                    inputStreams[i/2] = new BufferedInputStream(urlConnection.getInputStream());
+                    inputStreams[i / 2] = new BufferedInputStream(urlConnection.getInputStream());
                 }
 
                 String[] queryResponses = new String[4];
@@ -230,7 +208,7 @@ public class PlanActivity extends AppCompatActivity {
         protected void onPostExecute(String[] results) {
             try {
                 // parse request result into JSON object
-                for (String result: results) {
+                for (String result : results) {
                     JSONObject nameQueryResult = new JSONObject(result);
 
                     // get the name of the location from the JSON object and add it to ArrayList
@@ -241,11 +219,9 @@ public class PlanActivity extends AppCompatActivity {
 
                 // Start preferences activity, while passing down destinations data
                 Intent preferencesIntent = new Intent(getApplicationContext(), PreferencesActivity.class);
-
                 preferencesIntent.setAction(Intent.ACTION_SEND_MULTIPLE);
                 preferencesIntent.putStringArrayListExtra(Intent.EXTRA_STREAM, (ArrayList<String>) destinations);
                 preferencesIntent.setType("possibleDestinations");
-
                 startActivity(preferencesIntent);
             } catch (Exception e) {
                 // TODO: handle errors
